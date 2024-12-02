8c60a2071d97fcc2b49cc076fc14151efb51f1fa
IT21811272 <yomal000siri@gmail.com>
2024-12-03 01:45:01 +0530

    Add Profile Button

--- src/pages/Levels.jsx
+++ src/pages/Levels.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './levels.css';
 import { auth } from '../firebase'; // Importing auth from Firebase
 import Navbar from '../components/navbar/Navbar';
-
-
 
 const Levels = ({ user, username }) => {
   const navigate = useNavigate();
@@ -31,19 +28,24 @@ const Levels = ({ user, username }) => {
   return (
     <div>
       <Navbar username={username} /> {/* Pass the username to Navbar */}
-    <div className="levels">
-      
+      <div className="levels">
+        <form id="levelForm">
+          <h2 id="d">BABY ELEPHANT</h2>
+          <h2 id="e">Forest Math Mission</h2>
+          <Link to="/game">
+            <button id="Easy-btn">Game Start</button>
+          </Link>
 
-      <form id="levelForm">
-        <h2 id="d">BABY ELEPHANT</h2>
-        <h2 id="e">Forest Math Mission</h2>
-        <Link to="/game">
-          <button id="Easy-btn">Game Start</button>
-        </Link>
-        <button id="Medium-btn" onClick={handleLogout}>Log Out</button>
-        <div className="imgs" />
-      </form>
-    </div>
+          {/* Profile Button */}
+          <Link to="/profile">
+            <button id="Profile-btn">Profile</button>
+          </Link>
+
+          <button id="Medium-btn" onClick={handleLogout}>Log Out</button>
+          
+          <div className="imgs" />
+        </form>
+      </div>
     </div>
   );
 }
