66299dab9509c4f7bca6b38abf4dd952579251d5
IT21811272 <yomal000siri@gmail.com>
2024-12-04 22:59:49 +0530

    change home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="home">
       <div id="homeForm">
-        <h1 id="a">Welcome to the Game</h1>
+        <h1 id="a">BABY ELEPHANT</h1>
         <h2 id="b">Please Log in or Register to Continue</h2>
 
         <Link to="/login">
